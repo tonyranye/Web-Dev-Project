f762068eb096f1b7b59e89a5ba4d9c68e4617141
isaacDJ-tech <249901873+isaacDJ-tech@users.noreply.github.com.>
2026-04-09 14:47:53 -0400

    Halfway done Basic Design for login and Sign up

---
+++ fitness-tracking-app/src/views/signup.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class = "signup">
+<h1>{{ formTitle }}</h1>
+
+<div v-if="isSignUp">
+
+<form>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <label for="password"> Password:</label>
+    <input type="password" id="password" name= "password" required>
+    <button type= "submit"> Sign up</button>
+    <button type="button" @click="toggleForm">Already have an account? Log in</button>
+</form>
+
+</div>
+
+<div v-else>
+<form>
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <label for="password"> Password:</label>
+    <input type="password" id="password" name= "password" required>
+    <button type= "submit"> Log in</button>
+    <button type="button" @click="toggleForm">Don't have an account? Sign up</button>
+</form>
+
+</div>
+
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return{
+            isSignUp: true
+        }
+    },
+    methods: {
+        toggleForm(){
+            this.isSignUp = !this.isSignUp;
+        }
+    },
+    computed: {
+        formTitle(){
+            return this.isSignUp ? 'Sign Up' : 'Log In';
+        }
+    }
+}
+
+</script>
+
+
+
+
+<style></style>
